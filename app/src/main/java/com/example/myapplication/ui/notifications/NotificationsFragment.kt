cc19ea5ad8dfa2a62a460e62e2dcc0ddc19602a8
kgy1008 <gayeon1008@g.skku.edu>
2024-01-02 20:36:52 +0900

    feat: 허용 요청

--- app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.kt
@@ -1,101 +1,125 @@
 package com.example.myapplication.ui.notifications
 
-import android.Manifest
-import android.app.AlertDialog
+import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.core.content.ContextCompat
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentNotificationsBinding
+import com.example.myapplication.ml.ModelUnquant
+import com.example.myapplication.ui.gallery.Image
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class NotificationsFragment : Fragment() {
 
-    private val PERMISSION_CODE = 1001
+    private var _binding: FragmentNotificationsBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var selectBtn: Button
+    private lateinit var predBtn: Button
+    private lateinit var resView: TextView
+    private lateinit var testImage: ImageView
+
+    private lateinit var bitmap: Bitmap
+    var images: ArrayList<Image> = ArrayList()
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_notifications, container, false)
+    ): View {
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-        val selectBtn: Button = view.findViewById(R.id.selectBtn)
+        val notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        // 뷰바인딩을 사용하여 뷰 참조
+        selectBtn = binding.selectBtn
+        predBtn = binding.predictBtn
+        resView = binding.resView
+        testImage = binding.testImage
+
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+            .build()
+
+        // 사진 선택 버튼
         selectBtn.setOnClickListener {
-            checkPermissionAndSelectImage()
+            /*val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            activityResult.launch(intent)*/
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            //intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            activityResult.launch(intent)
+
         }
 
-        return view
-    }
+        // 분석 시작 버튼
+        predBtn.setOnClickListener {
 
-    private fun checkPermissionAndSelectImage() {
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            // 이미 권한이 허용되어 있을 때 이미지 선택 로직을 실행합니다.
-            // 여기에 이미지 선택 로직을 넣으시면 됩니다.
-        } else {
-            requestPermissions(
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                PERMISSION_CODE
-            )
-        }
-    }
+            // 이미지 전처리 과정
+            var tensorImage = TensorImage(DataType.FLOAT32)
+            tensorImage.load(bitmap)
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 권한이 허용되면 권한이 부여된 후 이미지 선택 로직을 실행합니다.
-                    // 여기에 이미지 선택 로직을 넣으시면 됩니다.
-                } else if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    showPermissionAlertDialog()
-                } else {
-                    goSettingActivityAlertDialog()
+            tensorImage = imageProcessor.process(tensorImage)
+
+            val model = ModelUnquant.newInstance(requireContext())
+            val inputFeature0 =
+                TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+            inputFeature0.loadBuffer(tensorImage.buffer)
+
+            val outputs = model.process(inputFeature0)
+            val outputFeatureO = outputs.outputFeature0AsTensorBuffer.floatArray
+
+            // 결과를 정렬
+            val sortedResults = outputFeatureO.mapIndexed { index, fl -> index to fl }
+                .sortedByDescending { (_, fl) -> fl }
+
+            // 정렬된 결과를 출력
+            val labels = arrayOf("cat", "dog", "bear")
+            val resultText = buildString {
+                sortedResults.forEach { (index, fl) ->
+                    val label = labels[index]
+                    append("$label: %.1f%%\n".format(fl * 100))
                 }
             }
+
+            resView.text = resultText
+            model.close()
+        }
+
+        return root
+    }
+    private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // 하나의 이미지가 선택된 경우
+            val imageUri = result.data?.data
+            bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, imageUri)
+            testImage.setImageBitmap(bitmap)
         }
     }
-
-    private fun showPermissionAlertDialog() {
-        AlertDialog.Builder(requireContext())
-            .setTitle("권한 승인이 필요합니다.")
-            .setMessage("사진을 선택 하시려면 권한이 필요합니다.")
-            .setPositiveButton("허용하기") { _, _ ->
-                requestPermissions(
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    PERMISSION_CODE
-                )
-            }
-            .setNegativeButton("취소하기") { _, _ -> }
-            .create()
-            .show()
-    }
-
-    private fun goSettingActivityAlertDialog() {
-        AlertDialog.Builder(requireContext())
-            .setTitle("권한 승인이 필요합니다.")
-            .setMessage("앨범에 접근 하기 위한 권한이 필요합니다.\n권한 -> 저장공간 -> 허용")
-            .setPositiveButton("허용하러 가기") { _, _ ->
-                val goSettingPermission = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                goSettingPermission.data = Uri.parse("package:" + requireActivity().packageName)
-                startActivity(goSettingPermission)
-            }
-            .setNegativeButton("취소") { _, _ -> }
-            .create()
-            .show()
-    }
 }
+
+
